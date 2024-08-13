5e8b9b68b11a46027d20bab57464143a23f930ee
kokolopo <kokolopo321@gmail.com>
2024-08-14 01:12:58 +0700

    s

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Middleware untuk parsing body (optional, jika Anda ingin menerima data POST)
 app.use(express.json());
 
 // Route untuk set cookie
-app.get('/set-cookie', (req, res) => {
+app.get("/set-cookie", (req, res) => {
   // Mengatur cookie
-  res.cookie('myCookie', 'cookieValue', {
-    domain: 'simasda.kotabogor.go.id',  // Sesuaikan dengan domain Anda
-    path: '/',                 // Path di mana cookie berlaku
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari dalam milidetik
-    httpOnly: true,            // Cookie hanya dapat diakses oleh server (opsional)
-    secure: true,              // Cookie hanya dikirim melalui HTTPS (opsional)
-    sameSite: 'Lax'            // Mencegah pengiriman cookie lintas situs (opsional)
-  });
+  res.cookie(
+    "WhatEver",
+    "90b861435aa7944225649fc98859833407abc857c5d131ffefdd48199bd116d46c3cd066",
+    {
+      domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
+      path: "/", // Path di mana cookie berlaku
+      sameSite: "Lax", // Mencegah pengiriman cookie lintas situs (opsional)
+    }
+  );
+  res.cookie(
+    "auth_tkt",
+    "d0ba1a94113b756d42397750cee766840345154db2905dcb7ff43affd9ad000319a9accea787b094b14f1d653368bb62d7d4079e57dd4a9fb17f3b03c513ef7366bba189197!userid_type:int",
+    {
+      domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
+      path: "/", // Path di mana cookie berlaku
+      sameSite: "Lax", // Mencegah pengiriman cookie lintas situs (opsional)
+    }
+  );
 
   // Mengirimkan respon
-  res.send('Cookie has been set!');
+  res.send("Cookie has been set!");
 });
 
 // Start server
