f64e480b48f1dad4d78d2cb44b2c57e27664dde6
kokolopo <kokolopo321@gmail.com>
2024-08-14 01:42:00 +0700

    s

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/set-cookie", (req, res) => {
     "WhatEver",
     "90b861435aa7944225649fc98859833407abc857c5d131ffefdd48199bd116d46c3cd066",
     {
-      domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
+      //domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
       path: "/", // Path di mana cookie berlaku
       sameSite: "Lax", // Mencegah pengiriman cookie lintas situs (opsional)
     }
@@ -20,7 +20,7 @@ app.get("/set-cookie", (req, res) => {
     "auth_tkt",
     "d0ba1a94113b756d42397750cee766840345154db2905dcb7ff43affd9ad000319a9accea787b094b14f1d653368bb62d7d4079e57dd4a9fb17f3b03c513ef7366bba189197!userid_type:int",
     {
-      domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
+      //domain: "simasda.kotabogor.go.id", // Sesuaikan dengan domain Anda
       path: "/", // Path di mana cookie berlaku
       sameSite: "Lax", // Mencegah pengiriman cookie lintas situs (opsional)
     }
